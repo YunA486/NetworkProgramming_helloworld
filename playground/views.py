f1dfa03df2a551a878ecc1c5ddb36eaec285686b
YunA486 <w2039@e-mirim.hs.kr>
2022-03-21 14:24:18 +0900

    templates: playground/ -> hello_html/ -> say_hello_html() -> html

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def say_hello(request):
     return HttpResponse("Hello, World!")
+
+
+def say_hello_html(request):
+    return render(request, 'playground/hello.html')
+
